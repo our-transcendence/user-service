794183ce00a8e43e5b79f24b1f8d0a508edf221f
beroux <42benoit.roux@gmail.com>
2024-04-24 08:56:35 +0200

    Remove picture from db and set good attributes to User model id

--- user-service/user/models.py
+++ user-service/user/models.py
@@ -5,11 +5,10 @@ from django.core.validators import MinLengthValidator
 
 
 class User(models.Model):
-    id = models.BigIntegerField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.BigIntegerField(primary_key=True)
     login = models.CharField(max_length=15, unique=True)
     displayName = models.CharField(
         max_length=25,
         validators=[MinLengthValidator(5, "Must contains at least 5 char")],
         null=True
     )
-    picture = models.CharField(max_length=25, null=True)
