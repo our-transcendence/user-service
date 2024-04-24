fda57fa75280b093272cf8cf7cdc7ec63a29c2a7
beroux <42benoit.roux@gmail.com>
2024-04-24 08:57:34 +0200

    Remove display name from expected user's data and add id

--- user-service/user/endpoints.py
+++ user-service/user/endpoints.py
@@ -14,17 +14,17 @@ def create_user(request):
     except json.JSONDecodeError:
         return response.HttpResponse(status=400, reason="Bad Json content: Decode Error")
 
-    expected_keys = {"login", "display_name"}
+    expected_keys = {"id", "login"}
     if set(data.keys()) != expected_keys:
         return response.HttpResponse(status=400, reason="Bad Json content: Bad Keys")
 
+    user_id = data["id"]
     login = data["login"]
-    display_name = data["display_name"]
 
     if User.objects.filter(login=login).exists():
         return response.HttpResponse(status=401, reason="User with this login already exists")
 
-    new_user = User(login=login, displayName=display_name)
+    new_user = User(id=user_id, login=login, displayName=login)
     new_user.save()
 
     return response.HttpResponse(status=200)
