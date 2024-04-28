9dc6060ed5c9d0bf712f7bf026108196d02e5438
Guillaume Rodriguez <guillaume.rodriguez@epita.fr>
2024-04-29 00:11:55 +0200

    Friend Model

--- user-service/user/models.py
+++ user-service/user/models.py
@@ -12,3 +12,12 @@ class User(models.Model):
         validators=[MinLengthValidator(5, "Must contains at least 5 char")],
         null=True
     )
+
+class Friend(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friends')
+    friend = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friends_with')
+    created_at = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        unique_together = ('user', 'friend')
+    def __str__(self):
+        return f"{self.user.login} is friends with {self.friend.login}"
